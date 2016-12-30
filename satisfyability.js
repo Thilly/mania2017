fee03a0474e58ce68e94c8e283b0d3e66c0af912
Thilly <nicholas.evans1@gmail.com>
2016-12-29 20:58:59 -0800

    wip

--- satisfyability.js
+++ satisfyability.js
@@ -1,72 +1,88 @@
-'use strict';
-
-/**
- *  The functions are 'plotted' in a duration and the duration is chopped up.
- *  Each plank of the duration is checked for satisfyability.
- *  If M is the number of planks
- *  If N is the number of functions
- *  O(N*M) for satisfyability?
- */
-
-var STEPS = 100;               // resolution of satisfyability
-var DELTA_T = 1 / STEPS;       // plank of time between start and end
-var REQUIRED_CONFIDENCE = .2;  // sigmoid training, use between zero 1
-var NUM_FAKE_FUNCS = 1;       // how many 'noise' functions to add
-
-var FUNCTIONS = [              // few generic functions
-  function justX (timeValue) {
-    return timeValue;
+var LAWS_OF_UNIVERSE = [    // one day these wont be generic functions
+  function justX (x) {
+    return x;
   },
-  function xSquared (timeValue) {
-    return timeValue * timeValue;
+  function xSquared (x) {
+    return x * x;
   },
-  function xCubed (timeValue) {
-    return timeValue * timeValue * timeValue;
+  function xCubed (x) {
+    return x * x * x;
   },
-  function somethingDifferent(timeValue) {
-    return timeValue - Math.log(timeValue);
+  function somethingDifferent(x) {
+    return x - Math.log(x);
   },
-  function squareRoot(timeValue) {
-    return Math.sqrt(timeValue);
+  function squareRoot(x) {
+    return Math.sqrt(x);
   }
 ];
-
-function getZeta() {
-  var fakeFunctions = FUNCTIONS;
-  for (var i = 0; i < NUM_FAKE_FUNCS; i++) {
-    fakeFunctions.push(function(timeValue){
+var UNIVERSE = 1;  // “I think; therefore I am.” ― René Descartes
+var PLANKS = 100;  // “Every one of us is, in the cosmic perspective, precious. If a human disagrees with you, let him live. In a hundred billion galaxies, you will not find another.” ― Carl Sagan, Cosmos
+var beginning_of_universe = UNIVERSE / PLANKS;  // The goal of life is living in agreement with nature. - Zeno of Ela
+var objective_history = observe_reality();
+function observe_reality() {
+  var SUBJECTIVE_REALITIES = 1;   // there is noise in reality
+  var objective_history = [];  // start with nothing
+  for (var i = 0; i < SUBJECTIVE_REALITIES; i++) {
+    objective_history.push(function(objectivity_is_unused) {
       return 1 / (1 + Math.E^-(Math.random() * 2))
     });
   }
-  return fakeFunctions;
+  return objective_history + LAWS_OF_UNIVERSE;  // already there, just need to read it
 }
 
-function averageFunction(currentFunction, timeValue) {
-  return (currentFunction(timeValue) + currentFunction(timeValue + DELTA_T)) / 2;
-}
+var START_DEPTH = 0;  // begin at birth, niave, no understanding of world
+var DIRECTION = 1;    // start at the end, to solve the now
+var RATE_OF_CHANGE = 1/2;  // geometric refinement, adjust your reality to fit objective reality
+satisfy_goals(objective_history, beginning_of_universe, DIRECTION, RATE_OF_CHANGE, START_DEPTH);
 
-function confidenceChecker (outputValue) {
-  // is the value from function 'true'
-  return outputValue > REQUIRED_CONFIDENCE
-}
+function satisfy_goals(observations, start_plank, direction, scale, depth) {
+  var SUCCESS_THRESHOLD = .2; // you decide what that is
+  var success = true;  // you can do it
+  for (var plank = start_plank; 0 < plank < 1; plank += (scale * direction)) {
+    observations = [];  // always start over
+    var improving = true;  // just by trying you are doing better
 
-function satisfy() {
-  var zeta = getZeta(10);
-  for(var step = 0; step < STEPS; step++) {
-    var satisfied = true;
-    var values = [];
-    for(var i = 0; i < zeta.length; i++) {
-      var value = averageFunction(zeta[i], step * DELTA_T);
-      values.push(value);
-      satisfied = satisfied && confidenceChecker(value);
+    for(var i = 0; i < observations.length; i++) {
+      var past_objective_reality = observations[i](plank*scale);
+      var future_objective_reality = observations[i](plank*scale + scale);
+      var present_objective_reality = past_objective_reality + future_objective_reality / 2;
+      observations.push(present_objective_reality);
+      improving = subjective_improvement_threshold(observations);
+      function subjective_improvement_threshold(outputValue) {
+        var BETTER = .2;  // what value are you ok with? free will.
+        return outputValue > GOOD_ENOUGH_FOR_ME;
+      }
     }
+
+    if (improving && subjective_success_threshold(value)) {
+
+    }
+      function subjective_success_threshold(outputValue, satisfied) {
+        // is the value from function 'true'
+        return outputValue > SUCCESS_THRESHOLD
+      }
+
+    var subjective_success = subjective_success_threshold()
+    function subjective_success_threshold(outputValue) {
+        var GOOD_ENOUGH_FOR_ME = .4;  // what value are you ok with? still free will.
+        return outputValue > GOOD_ENOUGH_FOR_ME;
+      }
     if (satisfied) {
-      return step;
+      var next_start_step = step + (scale * direction);
+      var next_direction = direction * -direction;
+      if (depth < MAX_DEPTH) {
+        return satisfy(next_start_step, next_direction, scale * GEOMETRIC, depth++);
+      }
     }
-    console.log(values);
+    if (depth === 0) {  // Making your mark on the world is hard. If it were easy, everybody would do it. But it’s not. It takes patience, it takes commitment, and it comes with plenty of failure along the way. The real test is not whether you avoid this failure, because you won’t. It’s whether you let it harden or shame you into inaction, or whether you learn from it; whether you choose to persevere. -Barack Obama
+      satisfied = true;
+    }
   }
-  return satisfied;
+  return {
+    'timeStart' : step,
+    'timeEnd' : step + scale,
+    'satisfied': depth > 0,
+    'values' : values
+  }
 }
-
-satisfy();
 
