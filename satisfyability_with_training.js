fb7d6a5feebe4ba220717a68824e82c8a4ba4433
Thilly <nicholas.evans1@gmail.com>
2016-12-31 01:01:22 -0800

    wip

--- satisfyability_with_training.js
+++ satisfyability_with_training.js
@@ -1,5 +1,5 @@
 var UNIVERSE = function() {
-  var SEED = Math.random();
+  var SEED = Math.random();  // “The beginning is the most important part of the work.” ― Plato, The Republic
   var DURATION = 1;
 
   this.UNIVERSAL_CONSTANT = function(plank_duration) {
@@ -8,10 +8,14 @@ var UNIVERSE = function() {
 
   this.UNIVERSAL_LINEAR = function(plank_duration) {
     return plank_duration + this.UNIVERSAL_CONSTANT(plank_duration);
-  };
+  }; // "I want to build a Taj Mahal that will never get its last tile."
   this.UNIVERSAL_SQUARE = function(plank_duration) {
     return plank_duration * plank_duration + this.UNIVERSAL_LINEAR(plank_duration);
   };
+  this.UNIVERSAL_CUBE = function(plank_duration) {
+    return plank_duration * plank_duration * plank_duration + this.UNIVERSAL_LINEAR(plank_duration);
+  };
+  // "yadda yadda yadda" - Elaine
 
   this.UNIVERSAL_DURATION = function(plank_duration) {
     return DURATION;
@@ -27,18 +31,19 @@ var UNIVERSE = function() {
       this.UNIVERSAL_CONSTANT(plank_duration),
       this.UNIVERSAL_LINEAR(plank_duration),
       this.UNIVERSAL_SQUARE(plank_duration),
+      this.UNIVERSAL_CUBE(plank_duration),
       this.UNIVERSAL_ENERGY(plank_duration)
     ];
   }
-}; // “If you wish to make an apple pie from scratch, you must first invent the universe.” ― Carl Sagan, Cosmos
-var BEGINNING = 0;  //
-var reality = new UNIVERSE();  // “I think; therefore I am.” ― René Descartes
-var plank_duration = reality.UNIVERSAL_DURATION();
+}; // “If you wish to make an apple pie from scratch, you must first invent the universe.” ― Carl Sagan
+var BEGINNING = 0;  // “Isn't it nice to think that tomorrow is a new day with no mistakes in it yet?” ― L.M. Montgomery
+var reality = new UNIVERSE();  // "My goal is simple. It is a complete understanding of the universe, why it is as it is and why it exists at all." - Stephen Hawking
+var plank_duration = reality.UNIVERSAL_DURATION();  // “I think; therefore I am.” ― René Descartes
 var HUMAN_PLANKS = 12016;  // "https://www.youtube.com/watch?v=czgOWmtGVGs" -Kurzgesagt – In a Nutshell
-var reality_plank_duration = plank_duration / HUMAN_PLANKS;
+var reality_plank_duration = plank_duration / HUMAN_PLANKS;  // “A story has no beginning or end: arbitrarily one chooses that moment of experience from which to look back or from which to look ahead.” - Graham Greene
 var beginning_of_reality = reality_plank_duration;  // The goal of life is living in agreement with nature. - Zeno of Ela
-var OBJECTIVE_HISTORY = observe_reality(beginning_of_reality);  //
-function observe_reality(plank_duration) {
+var OBJECTIVE_HISTORY = observe_reality(beginning_of_reality);  // "I explore the world using science." - Destin
+function observe_reality(plank_duration) {  // Seeing and knowing everything is boring, I want to experience it
   return reality.OBJECTIVE_OBSERVATION(plank_duration);
 }
 
@@ -47,7 +52,7 @@ function statistically_predict_subjective_goal(plank_duration, plank_step, depth
   var SUBJECTIVE_SUCCESS = .7;
   var SUBJECTIVE_IMPROVEMENT = .4;  // what value are you ok with? free will.
   var MAX_DEPTH_WILLING_TO_LOOK = 2;  //
-  var subjective_sucess_probability = 1;  // “I believe in everything until it's disproved. So I believe in fairies, the myths, dragons. It all exists, even if it's in your mind. Who's to say that dreams and nightmares aren't as real as the here and now?” ― John Lennon
+  var subjective_success_probability = 1;  // “I believe in everything until it's disproved. So I believe in fairies, the myths, dragons. It all exists, even if it's in your mind. Who's to say that dreams and nightmares aren't as real as the here and now?” ― John Lennon
   var subjective_improvement_probability = 1; //
 
   var subjective_history = observe_subjective_reality(plank_duration); // [Q1, q2, q43 ]
@@ -67,7 +72,7 @@ function statistically_predict_subjective_goal(plank_duration, plank_step, depth
     return subjective_history.reduce(function(a, b){ return a + b}) * 1/subjective_history.length
   }
 
-  var subjective_future_goal = look_inward(plank_duration, plank_step, depth+1);  //think
+  var subjective_future_goal = look_inward(plank_duration, plank_step, depth+1);
   function look_inward(inward_plank_duration, inward_plank_step, inward_depth) {
     if (inward_depth > MAX_DEPTH_WILLING_TO_LOOK) {
       return {};
@@ -76,7 +81,7 @@ function statistically_predict_subjective_goal(plank_duration, plank_step, depth
     var pondered_observations = null;
     var inward_space_time = plank_duration * 1/(1 + plank_step);
     subjective_improvement_probability = 1;
-    do {
+    do { // "Persistence is very important. You should not give up unless you are forced to give up." - Elon Musk
       //console.log('inward_plank_duration: ' + inward_plank_duration);
       ////console.log('inward_plank_step: ' + inward_plank_step);
       ////console.log('inward_depth: ' + inward_depth);
@@ -98,25 +103,25 @@ function statistically_predict_subjective_goal(plank_duration, plank_step, depth
       var subjective_prediction = subjective_present_trend - subjective_history_trend;
       console.log('subjective_prediction: ' + subjective_prediction);
 
-      new_predictions.push(subjective_prediction);//multi dimensional spin
+      new_predictions.push(subjective_prediction);  // "The dude abides" - Lebowski
 
       subjective_improvement_probability = find_subjective_improvement_probability(subjective_prediction);
       function find_subjective_improvement_probability(predictions) {
         return aggregate_history(predictions);
       }
-      subjective_sucess_probability = (subjective_sucess_probability + find_subjective_success_value(new_predictions)) * .5;
+      subjective_success_probability = (subjective_success_probability + find_subjective_success_value(new_predictions)) * .5;
       function find_subjective_success_value(predictions) {
         return aggregate_history(predictions);
       }
-      console.log('subjective_sucess_probability: ' + subjective_sucess_probability);
+      console.log('subjective_success_probability: ' + subjective_success_probability);
       console.log('subjective_improvement_probability: ' + subjective_improvement_probability);
 
       var decision = null;
-      if (subjective_improvement_probability > SUBJECTIVE_IMPROVEMENT) {
+      if (subjective_improvement_probability > SUBJECTIVE_IMPROVEMENT) {  //
         decision = decide();
         function decide() {
           pondered_observations = look_inward(inward_plank_duration, inward_plank_step, inward_depth + 1);
-          if (pondered_observations['success'] > SUBJECTIVE_SUCCESS) {
+          if (pondered_observations['success'] > SUBJECTIVE_SUCCESS) {  // "Will it chooch" -Ave
             return pondered_observations;
           }
           if (pondered_observations['improving'] > SUBJECTIVE_IMPROVEMENT) {
@@ -131,7 +136,7 @@ function statistically_predict_subjective_goal(plank_duration, plank_step, depth
       subjective_history.push({
         'plank_duration': inward_plank_duration,
         'plank_step': inward_plank_step,
-        'success': subjective_sucess_probability,
+        'success': subjective_success_probability,
         'improving': subjective_improvement_probability,
         'observations': subjective_history,
         'depth': inward_depth
@@ -142,7 +147,7 @@ function statistically_predict_subjective_goal(plank_duration, plank_step, depth
     return pondered_observations || {
       'plank_duration': plank_duration,
       'plank_step': plank_step,
-      'success': subjective_sucess_probability,
+      'success': subjective_success_probability,
       'improving': subjective_improvement_probability,
       'observations': subjective_history,
       'depth': depth
@@ -160,7 +165,7 @@ console.log('Probability of improving in this plank: ' + reason['improving']);
 console.log('Volume of this plank:' + reason['plank_duration']);
 console.log('Universal seed:' + reality.UNIVERSAL_CONSTANT(reason['plank_duration']));
 
-// “Every one of us is, in the cosmic perspective, precious. If a human disagrees with you, let him live. In a hundred billion galaxies, you will not find another.” ― Carl Sagan, Cosmos
+// “Every one of us is, in the cosmic perspective, precious. If a human disagrees with you, let him live. In a hundred billion galaxies, you will not find another.” ― Carl Sagan
 // “People are afraid of themselves, of their own reality; their feelings most of all. People talk about how great love is, but that’s bullshit. Love hurts. Feelings are disturbing. People are taught that pain is evil and dangerous. How can they deal with love if they’re afraid to feel? Pain is meant to wake us up. People try to hide their pain. But they’re wrong. Pain is something to carry, like a radio. You feel your strength in the experience of pain. It’s all in how you carry it. That’s what matters. Pain is a feeling. Your feelings are a part of you. Your own reality. If you feel ashamed of them, and hide them, you’re letting society destroy your reality. You should stand up for your right to feel your pain.” ― Jim Morrison
 // “You know you're in love when you can't fall asleep because reality is finally better than your dreams.” ― Dr. Seuss
 // "Go and do amazing things." - Thilly
