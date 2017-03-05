6e13ac2617bb5d2de1ff6d5974879982c5daef89
Thilly <nicholas.evans1@gmail.com>
2017-03-04 21:55:17 -0800

    wip

---
+++ Programs/modules/rational.js
@@ -0,0 +1,102 @@
+// todo: decide on mutablility, currently operations modifies object instead of creating new
+
+module.exports = Rational;
+
+function Rational(in_value) {
+
+  var in_rational = checkValue(in_value);
+  this.numer = in_rational.numer;
+  this.denom = in_rational.denom;
+
+  this.add = function(in_value) {
+    var oper_rational = checkValue(in_value);
+    if(this.denom != oper_rational.denom) {
+      var inNumerator = this.denom * oper_rational.numer;
+      this.numer *= oper_rational.denom;
+      this.numer += inNumerator;
+      this.denom *= oper_rational.denom;
+      this.reduce();
+    } else {
+      this.numer += oper_rational.numer;
+      this.reduce();
+    }
+    return this;
+  };
+
+  this.sub = function(in_value) {
+    var oper_rational = checkValue(in_value);
+    if(this.denom != oper_rational.denom) {
+      var inNumerator = this.denom * oper_rational.numer;
+      this.numer *= oper_rational.denom;
+      this.numer -= inNumerator;
+      this.denom *= oper_rational.denom;
+      this.reduce();
+    } else {
+      this.numer -= oper_rational.numer;
+      this.reduce();
+    }
+    return this;
+  };
+
+  this.mult = function(in_value) {
+    var oper_rational = checkValue(in_value);
+    this.numer *= oper_rational.numer;
+    this.denom *= oper_rational.denom;
+    this.reduce();
+    return this;
+  };
+
+  this.divide = function(in_value){
+    var oper_rational = checkValue(in_value);
+    this.numer *= oper_rational.denom;
+    this.denom *= oper_rational.numer;
+    this.reduce();
+    return this;
+  };
+
+  this.reduce = function() {
+    var topRange = Math.sqrt(this.denom);
+    for(var i = 2; i < topRange, i < this.denom; i++) {
+      if(this.numer % i === 0 && this.denom % i === 0) {
+        this.numer /= i;
+        this.denom /= i;
+        i--;
+      }
+    }
+    return this;
+  };
+
+  this.toString = function() {
+    return this.numer + '/' + this.denom;
+  };
+
+  function checkValue(rational) {
+    if (isNumeric(rational)) {
+      if (rational.toString().split('.').length === 2) {
+        var whole = rational.toString().split('.')[0];
+        var numer = rational.toString().split('.')[1];
+        var denom = Math.pow(10, numer.length);
+        return {
+          numer: parseInt(numer) + parseInt(whole * denom),
+          denom: denom
+        };
+      } else {
+        return {
+          numer: in_value,
+          denom: 1
+        };
+      }
+    } else if(isNumeric(rational.numer) && isNumeric(rational.denom)) {
+      if (rational.denom === 0) {
+        throw 'rationals shouldn\'t have 0 as denominator';
+      }
+      return rational;
+    } else {
+      throw 'cannot parse ' + in_value + ' to a rational';
+    }
+  }
+
+  function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
+}
---
+++ Programs/modules/tests/test_rational.js
@@ -0,0 +1,21 @@
+var Rational = require('../rational.js');
+
+var one = new Rational(1);
+var one_half = new Rational(1/2);  // 0.5 goes in, 5/10 comes out
+var one_third = new Rational({numer: 1, denom: 3});
+var one_quarter = new Rational(0.25);  // 0.25 goes in, 25/100 comes out
+
+console.log('i should print 1/1: ' + one);
+console.log('i should print 5/10: ' + one_half);
+console.log('i should print 1/2: ' + one_half.reduce());
+console.log('i should print 1/3: ' + one_third);
+console.log('i should print 25/100: ' + one_quarter);
+console.log('i should print 1/4: ' + one_quarter.reduce());
+
+console.log('i should still print 1/4: ' + one_quarter);  // while objects are modified by operations vv
+console.log('i should print 3/4: ' + one_half.add(one_quarter));
+console.log('i should print 1/2: ' + one_half.sub(one_quarter));
+console.log('i should print 1/8: ' + one_half.mult(one_quarter));
+console.log('i should print 1/2: ' + one_half.divide(one_quarter));
+
+
